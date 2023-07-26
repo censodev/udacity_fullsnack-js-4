c570c160d60620d75aa99fd35f38268768df419f
Phuong Bui <buiphuongaptutc@gmail.com>
2023-07-26 16:54:26 +0700

    update ci cfg

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://udacity-fullsnack-js-4.eba-pgr9mqsk.us-east-1.elasticbeanstalk.com:8080/api/v0',
 };
 
 /*
